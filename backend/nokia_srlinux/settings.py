0c33b46f9cbeafac1ea61801bf41ee0d9dfc0ab8
Pedro Rodrigues <49519460+pedromrodrigues@users.noreply.github.com>
2022-11-30 23:14:16 +0000

    Update settings.py

--- backend/nokia_srlinux/settings.py
+++ backend/nokia_srlinux/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8786)wk15gmoq@1l^3-gay3#2i)yjlnx8is_*li!p&$zu%9w+9'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
